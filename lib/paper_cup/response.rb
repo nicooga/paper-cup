275cd005e1c8264cdf3d14e8df9c294a819a1705
Mariano Matayoshi <matayoshi.mariano@gmail.com>
2015-08-12 10:28:55 -0300

    fixup! Added request class with tests

--- lib/paper_cup/response.rb
+++ lib/paper_cup/response.rb
@@ -5,19 +5,23 @@ module PaperCup
   class Response
     attr_accessor :status, :response
 
-    def initialize(response)
-      set_response(response[0..-4])
-      @status = response[-3..-1].to_i
+    def initialize(raw_response)
+      @raw_response = raw_response
+    end
+
+    def response
+      @response ||= begin
+        str = @raw_response[0..-4]
+        PaperCup.valid_json?(str) ? JSON.parse(str) : str
+      end
+    end
+
+    def status
+      @status ||= @raw_response[-3..-1].to_i
     end
 
     def success?
       status.between?(200, 299)
-    end
-
-    private
-
-    def set_response(response)
-      @response = PaperCup.valid_json?(response) ? JSON.parse(response) : response
     end
   end
 end
